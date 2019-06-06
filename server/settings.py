e501943499625ab420da937f3d43d2a16037d707
gurpreet singh <amansingh9569@gmail.com>
2019-06-06 21:15:42 +0530

    heroku commit

--- server/settings.py
+++ server/settings.py
@@ -115,7 +115,11 @@ USE_TZ = True
 
 APPEND_SLASH = False
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
